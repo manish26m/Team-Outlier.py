7148b00e80ba4e944c3655c1a85890f9cb023a39
vaibhavtamang@gmail.com <vaibhavtamang@gmail.com>
2024-08-29 04:12:05 +0530

    Pushed once more

---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div className='h-full w-full'>
+      <h1 className='heading'>Your personal health companion</h1>
+      <h1 className='sub'>AI-Powered Predictions, Real-time support <br /> and Personalized Care-AI at your fingertips</h1>
+      <button className='relative top-60 left-10'>Get started</button>
+    </div>
+  )
+}
+
+export default Hero
