778bb96b63e4cf0300eadd475f1d26699236f8ac
vaibhavtamang@gmail.com <vaibhavtamang@gmail.com>
2024-08-28 23:56:27 +0530

    Betichod

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
             <NavLink to='/about' className='flex flex-col items-center gap-1'>
-                <p>About</p>
+                <p>News&Feed</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
             <NavLink to='/contact' className='flex flex-col items-center gap-1'>
